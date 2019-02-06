0772ff0e6b89ef37e9a90cf29d1c496c9a22aa2f
Henok Tesfaye <henoktesfaye1@gmail.com>
2019-02-06 11:31:45 -0500

    fixed very dumb problem

--- DevOpsToday.MSTesting/Tests/ErrorViewModelsTests.cs
+++ DevOpsToday.MSTesting/Tests/ErrorViewModelsTests.cs
@@ -3,11 +3,13 @@ using DevOpsToday.Models;
 
 namespace DevOpsToday.MSTesting.Tests
 {
+  [TestClass]
   public class ErrorViewModelsTests
   {
+    [TestMethod]
     public void Test_ShowRequestIdMethod()
     {
-      var sut = new ErrorViewModelsTests() {RequestId = "Oak"};
+      var sut = new ErrorViewModel() {RequestId = "Oak"};
 
       Assert.IsTrue(sut.ShowRequestId);
     }
